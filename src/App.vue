b843440abf973444f232182c8a76209a13b352ca
Void-ux <77840397+Void-ux@users.noreply.github.com>
2023-05-27 21:19:20 +0800

    feat: loading icon on execute style: fonts refactor: indentaion to 2 spaces

--- src/App.vue
+++ src/App.vue
@@ -9,75 +9,81 @@ const EDITOR_DEFAULT = `PROCEDURE CountToN(n: INTEGER)
     NEXT i
 ENDPROCEDURE
 
-CALL CountToN(10)`
+CALL CountToN(10)
+`
 
 function consolify(code) {
-    // Prefixes every line with a "> "
-    let lines = []
-    for (let i of code.trim().split('\n')) {
-        lines.push(`> ${i}`)
-    }
+  // Prefixes every line with a "> "
+  let lines = []
+  for (let i of code.trim().split('\n')) {
+    lines.push(`>>> ${i}`)
+  }
 
-    return lines.join('\n');
+  return lines.join('\n');
 }
 
 async function executeCode(code) {
-    if (code === "") {
-        alert("You silly goose! You can't execute nothing!");
-        this.stdout = null;
-        return;
-    }
-    console.debug(`Executing: ${code}`)
-    let response = await fetch('/api/execute', {
-        method: "POST",
-        body: JSON.stringify({'code': code}),
-    }).then((response) => response.text());
-    // let status = response.status;
-    // response = await response.text()
-    console.debug(`Received: ${response}`);
+  if (code === "") {
+    alert("You silly goose! You can't execute nothing!");
+    this.stdout = null;
+    return;
+  }
+  this.rendering = true;
+  this.stdout = null;
 
-    this.stdout = response !== "" ? consolify(response) : 'No output'
+  console.debug(`Executing: ${code}`)
+  let response = await fetch('/api/execute', {
+    method: "POST",
+    body: JSON.stringify({'code': code}),
+  }).then((response) => response.text());
+  // let status = response.status;
+  // response = await response.text()
+  console.debug(`Received: ${response}`);
 
+  this.rendering = false;
+  this.stdout = response !== "" ? consolify(response) : 'No output'
 }
 
 export default {
-    name: 'app',
-    async mounted() {
-        const el = this.$refs.editor;
-        this.editor = monaco.editor.create(el, {
-            value: EDITOR_DEFAULT,
-            language: 'pseudocode',
-            theme: 'vs-dark',
-            fontSize: 18
-        });
-    },
-    data() {
-        return {
-            stdout: null
-        }
-    },
-    methods: {
-        executeCode
+  name: 'app',
+  async mounted() {
+    const el = this.$refs.editor;
+    this.editor = monaco.editor.create(el, {
+      value: EDITOR_DEFAULT,
+      language: 'pseudocode',
+      theme: 'vs-dark',
+      fontSize: 18
+    });
+  },
+  data() {
+    return {
+      stdout: 'Your console output will be redirected here',
+      rendering: false
     }
+  },
+  methods: {
+    executeCode
+  }
 };
 </script>
 
 <template>
-    <head>
-        <title>Pseudocode Interpreter</title>
-    </head>
+  <head>
+    <title>Pseudocode Interpreter</title>
+  </head>
 
-    <div class="flex gap-0 w-screen h-screen">
-        <div ref="editor" class="w-1/2 h-full">
-        </div>
+  <div class="flex gap-0 w-screen h-screen">
+    <div ref="editor" class="w-1/2 h-full">
+  </div>
 
-        <div class="flex flex-col gap-2 w-1/2 h-full px-10 pt-4 text-xl">
-            <p class="text-4xl text-center text-zinc-500 font-semibold">Console</p>
-            <hr class="w-60 self-center text-zinc-500 pb-2">
-            <pre><code class="whitespace-pre-wrap">{{ stdout || 'Your console output will be redirected here' }}</code></pre>
-            <button @click="executeCode(this.editor.getValue())" class="absolute w-32 h-16 bottom-1 left-1/2 bg-zinc-800 rounded-xl text-bold duration-200 ease-in transition-all hover:bg-zinc-700 hover:mb-1.5">
-                Execute 🚀
-            </button>
-        </div>
+    <div class="flex flex-col gap-2 w-1/2 h-full px-10 pt-4 text-xl">
+      <p class="text-4xl text-center text-zinc-400 font-semibold">Console</p>
+      <hr class="w-60 self-center text-zinc-500 pb-2">
+      <pre v-if="!rendering"><code class="whitespace-pre-wrap">{{ stdout }}</code></pre>
+      <div v-else class="w-40 h-40 border-zinc-800 border-t-green-600 border-8 border-solid relative rounded-full m-auto mt-40 animate-spin"></div>
+      <button @click="executeCode(this.editor.getValue())" class="m-auto relative mb-4 text-xl font-sans w-32 h-16 bg-zinc-800 rounded-xl text-bold duration-200 ease-in transition-all hover:bg-zinc-700 hover:mb-6">
+        Execute 🚀
+      </button>
     </div>
+  </div>
 </template>
